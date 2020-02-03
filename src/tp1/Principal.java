59baea85ab7e2324448f559ea27f3f59605fcb0f
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-02-03 15:57:46 +0000

    Renommé cv.java -> Cv.java

--- src/tp1/Principal.java
+++ src/tp1/Principal.java
@@ -1,10 +1,14 @@
 package tp1;
 
+/**
+ * TP1 Assurance Qualité
+ *
+ * @author Benjamin Dupont, Eric Pintea, Jérémie Bergeron
+ **/
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+            System.out.println("Hello, World !");
 	}
 
 }
