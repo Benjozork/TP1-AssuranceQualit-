91ebf39206cbb59e17d4e6e779766cb9f42d985e
benjozork <benjamin.dupont.984@gmail.com>
2020-04-17 18:06:50 -0400

    misc: Restructure project, add Gradle test tasks

---
+++ src/tp1_facture/tests/tp1/TestCommande.java
@@ -0,0 +1,14 @@
+package tp1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestCommande {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
