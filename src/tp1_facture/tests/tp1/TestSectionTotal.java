f40cffa4a6ab5c9dc886a20f1c866e3eff5dd6b5
Louis-Marcel Otsama Mba <louismarcelotsamba@gmail.com>
2020-04-19 01:27:20 -0400

    ajouter les tests pour la section total

---
+++ src/tp1_facture/tests/tp1/TestSectionTotal.java
@@ -0,0 +1,61 @@
+package tp1;
+
+import tp1.models.Commande;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSectionTotal {
+
+    Commande.Client[] clients = new Commande.Client[] {
+            new Commande.Client("Éric"),
+            new Commande.Client("Benjamin"),
+            new Commande.Client("Louis-Marcel"),
+    };
+
+    Commande.Plat[] plats = new Commande.Plat[] {
+            new Commande.Plat("Poutine", 5.00),
+            new Commande.Plat("Frites", 2.50),
+            new Commande.Plat("Hamburger", 3.50),
+            new Commande.Plat("Boisson", 1.75),
+    };
+
+    Commande.LigneCommande[] lignesCommande = new Commande.LigneCommande[] {
+            new Commande.LigneCommande(clients[0], plats[0], "1"),
+            new Commande.LigneCommande(clients[0], plats[3], "1"),
+            new Commande.LigneCommande(clients[1], plats[1], "1"),
+            new Commande.LigneCommande(clients[1], plats[2], "3"),
+            new Commande.LigneCommande(clients[2], plats[3], "1"),
+    };
+
+    Commande commandeAUtiliser = new Commande(clients, plats, lignesCommande);
+
+    @Test public void devrait_comprendre_une_section_total_avec_taxes() throws IOException {
+        File fichierOut = new File("fichier_test.txt");
+
+        Afficheur.ecrireFacture(commandeAUtiliser, fichierOut);
+
+        StringBuilder contenFichier = new StringBuilder();
+        BufferedReader bf = new BufferedReader(new FileReader(fichierOut));
+        String ligne = ""; while ((ligne = bf.readLine()) != null)
+            contenFichier.append(ligne).append("\n");
+
+        System.out.println(contenFichier.toString());
+
+        assertTrue(contenFichier.toString().contains("\n\n=== Grand total ===\n\n" +
+                                                     "Éric: 6.75$\n" +
+                                                     "Benjamin: 13.0$\n" +
+                                                     "Louis-Marcel: 1.75$\n" +
+                                                     "---\n" +
+                                                     "TPS: 1.07$\n" +
+                                                     "TVQ: 2.14$\n" +
+                                                     "TOTAL: 24.71$"));
+    }
+
+}
